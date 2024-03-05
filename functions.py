b48e71c81a11490a639e0cc9cc95f03aafde6962
ItsDanny <danny.desnoo@ddesnoo.nl>
2024-03-05 15:44:29 +0100

    Commit 05-03-2024 15:45
    First commit and start point for the space race project

---
+++ functions.py
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------------------------------
+# Project                      : SPACE RACE
+# Project collaborators        : Sogaand, Julian, Danny
+# Project objective            : Creating a digital board game based in the Terminal
+# Project documentation        :
+# Last change preformed (Date) : 05/03/2024 15:45
+# Last change preformed by     : Danny
+# Last change (Description)    : Created the header for the .py files.
+# ---------------------------------------------------------------------------------------
